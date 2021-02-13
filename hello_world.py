d59734a6532a8b1fafb8846afdd3b954786a9f40
Jonathan <jonathan.bustamante03@gmail.com>
2021-02-13 14:09:14 -0600

    first commit

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+print("Hello World")
+name = "Jonathan!"
+print("Hello", name)
+name = "Jonathan!"
+print("Hello" + name)
+name = 21
+print("Hello", name)
+name = 21
+print("Hello", + name)
+fave_food1 = "chicken wings"
+fave_food2 = "pasta"
+print("I love to eat {} and {}.".format(fave_food1, fave_food2))
+fave_food1 = "chicken wings"
+fave_food2 = "pasta"
+print(f"I love to eat {fave_food1} and {fave_food2}.")
