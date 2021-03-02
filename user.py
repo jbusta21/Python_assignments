c58f4295a6c78fd2e6806fb955257c84a3f10458
Jonathan <jonathan.bustamante03@gmail.com>
2021-03-01 21:33:37 -0600

    bankaccount.py

--- user.py
+++ user.py
@@ -1,36 +1,49 @@
 class User:
-    
     def __init__(self, name):
         self.name = name
-        self.account_balance = 0
+        self.account = BankAccount(int_rate=0.09, balance=0)
 
     def make_deposit(self, amount):
-        self.account_balance += amount
+        self.account.deposit(amount)
 
     def make_withdrawal(self, amount):
-        self.account_balance -= amount
+        self.account.withdraw(amount)
+        print(self.account.balance)
 
     def display_user_balance(self):
         print(f"User: {self.name}, Balance: ${self.account_balance}")
+        print(self.account.balance)
 
-Jonathan = User("Jonathan")
-Natalie = User("Natalie")
-Carmen = User("Carmen")
+    def yield_interest(self):
+        self.account.yield_interest()
+        return self
 
-Jonathan.make_deposit(100)
-Jonathan.make_deposit(10000)
-Jonathan.make_deposit(333)
-Jonathan.make_withdrawal(4000)
-Jonathan.display_user_balance()
 
-Natalie.make_deposit(500)
-Natalie.make_deposit(8000)
-Natalie.make_withdrawal(122)
-Natalie.make_withdrawal(344)
-Natalie.display_user_balance()
+class BankAccount:
+    def __init__(self, int_rate=0.09, balance=0):
+        self.int_rate = int_rate
+        self.balance = balance
 
-Carmen.make_deposit(700000)
-Carmen.make_withdrawal(555)
-Carmen.make_withdrawal(3333)
-Carmen.make_withdrawal(888)
-Carmen.display_user_balance()
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+
+    def withdraw(self, amount):
+        self.balance -= amount
+        return self
+
+    def display_account_info(self):
+        print(f"balance: ${self.balance}")
+        return self
+
+    def yield_interest(self):
+        if self.balance > 0:
+            self.balance = self.balance + self.balance * self.int_rate
+        return self
+
+
+BankAccount_1 = BankAccount(.09, 5500)
+BankAccount_2 = BankAccount(.09, 54320)
+
+BankAccount_1.deposit(330).deposit(3000).deposit(394949).withdraw(399).yield_interest().display_account_info()
+BankAccount_2.deposit(3848390).deposit(39494).withdraw(384).withdraw(384).withdraw(394).withdraw(55).yield_interest().display_account_info()
